8ff40b811118163395039e072fecd8c81b6794f3
Anastasia1102 <doitinyourownway.f@gmail.com>
2024-01-30 11:51:27 +0000

    results

---
+++ js/task-1.js
@@ -0,0 +1,12 @@
+"use strict";
+const categoryItems = document.querySelectorAll("li.item");
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach((categoryItem) => {
+  const categoryName = categoryItem.querySelector("h2").textContent;
+  const categoryItemsCount = categoryItem.querySelectorAll(".list").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Elements: ${categoryItemsCount}`);
+});
